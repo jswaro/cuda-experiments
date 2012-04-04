0d2640de28169ba58a26f13ffa951bd6de8ccf02
James Swaro <jswaro@tesla2.cs.ohiou.edu>
2012-04-04 13:48:54 -0400

    pushing levenstein

--- levenstein/levenstein.h
+++ levenstein/levenstein.h
@@ -1,10 +1,9 @@
-#ifndef MATRIX_H
-#define MATRIX_H
+#ifndef LEVENSTEIN_H
+#define LEVEHSTEIN_H
 
-#define TILE_SIZE 16
-#define TILES_PER_BLOCK 2
-#define MAX_BLOCKS 8
+#define STRIPE_MAX 512
+#define WARP_MAX 32
 
-void matrixMulCuda(float* m1, float* m2, float* &result, size_t size);
+void levensteinCuda(char* s1, char* s2, size_t* result, size_t n);
 
 #endif
